5bae02b74da7073c6ef9d53302764f2548edd2db
VIEIBRU <bruno.vieira@ext.mercedes-benz.io>
2021-08-25 18:19:46 +0100

    re-enabling login

--- src/main/java/com/passGenerator/PassGenarator/Pessoa/PessoaController.java
+++ src/main/java/com/passGenerator/PassGenarator/Pessoa/PessoaController.java
@@ -1,11 +1,8 @@
 package com.passGenerator.PassGenarator.Pessoa;
 
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
