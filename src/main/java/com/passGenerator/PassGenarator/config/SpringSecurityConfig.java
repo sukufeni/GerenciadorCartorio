d2319cd00313d954281350fe9cc15f45e58af035
VIEIBRU <bruno.vieira@ext.mercedes-benz.io>
2021-08-29 21:07:00 +0100

    v1 of multi user

--- src/main/java/com/passGenerator/PassGenarator/config/SpringSecurityConfig.java
+++ src/main/java/com/passGenerator/PassGenarator/config/SpringSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 
@@ -16,18 +18,28 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-				//.antMatchers(HttpMethod.GET, "/**").hasAnyRole("user", "admin") //full access to everyone
+				// .antMatchers(HttpMethod.GET, "/**").hasAnyRole("user", "admin") //full access
+				// to everyone
 				.antMatchers(HttpMethod.GET, "/senha/proximasenha").hasRole("admin")
 				.antMatchers(HttpMethod.GET, "/protocolo/all").hasRole("admin")
-				.antMatchers(HttpMethod.GET, "/pessoa/all").hasRole("admin")
-				.antMatchers(HttpMethod.GET, "/senhas/all").hasAnyRole("user", "admin")
-				.antMatchers(HttpMethod.POST, "/senhas/gerar").hasAnyRole("user", "admin")
+				.antMatchers(HttpMethod.GET, "/pessoa/all").hasRole("admin").antMatchers(HttpMethod.GET, "/senhas/all")
+				.hasAnyRole("user", "admin").antMatchers(HttpMethod.POST, "/senhas/gerar").hasAnyRole("user", "admin")
 				.antMatchers(HttpMethod.POST, "/pessoa/gerar").hasAnyRole("user", "admin")
 				.antMatchers(HttpMethod.POST, "/protocolo/gerar").hasAnyRole("user", "admin")
 				.antMatchers(HttpMethod.POST, "/protocolo/imprimir").hasAnyRole("admin")
 				.antMatchers(HttpMethod.GET, "/protocolo/tipos").hasAnyRole("user", "admin")
 				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated().and().httpBasic().and()
 				.exceptionHandling().accessDeniedPage("/error");
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+		final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+		manager.createUser(User.withDefaultPasswordEncoder().username("eron").password("master").roles("admin").build());
+		manager.createUser(User.withDefaultPasswordEncoder().username("default").password("default").roles("user").build());
+
+		return manager;
 	}
 
 	@Bean
