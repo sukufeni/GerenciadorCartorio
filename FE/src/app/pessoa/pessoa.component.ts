d2da8af3026d23a83478aea7b894f50d8c7747e0
VIEIBRU <bruno.vieira@ext.mercedes-benz.io>
2021-11-01 22:55:11 +0000

    clean

--- FE/src/app/pessoa/pessoa.component.ts
+++ FE/src/app/pessoa/pessoa.component.ts
@@ -42,21 +42,6 @@ export class PessoaComponent implements OnInit {
       }
     )
   }
-
-  public mascara(i){
-    console.log("got into!");
-     var v = i.value;
-     
-     if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
-        i.value = v.substring(0, v.length-1);
-        return;
-     }
-     
-     i.setAttribute("maxlength", "14");
-     if (v.length == 3 || v.length == 7) i.value += ".";
-     if (v.length == 11) i.value += "-";
-  
-  }
   
   public onOpenModal(pessoa: Pessoa | null, mode: string): void{
     const container = document.getElementById('main-container');
